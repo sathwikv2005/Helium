93a001bfe66d3f7f05ade73c2ea40673e11d453c
sathwikv2005 <sathwikv2005@gmail.com>
2026-01-23 23:32:53 +0530

    add: values

--- include/chunk.h
+++ include/chunk.h
@@ -2,8 +2,10 @@
 #define helium_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -11,10 +13,14 @@ typedef struct {
     int count;
     int capacity;
     uint8_t* code; /**< Bytecode array */
+    int* lines;
+    ValueArray constants;  // constant values storage
+
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,7 +5,15 @@
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
-    writeChunk(&chunk, OP_RETURN);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, addConstant(&chunk, 1.9), 1);
+    writeChunk(&chunk, OP_CONSTANT, 3);
+    writeChunk(&chunk, addConstant(&chunk, 1), 3);
+    writeChunk(&chunk, OP_RETURN, 4);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
