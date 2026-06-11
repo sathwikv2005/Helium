c7e51f23a46aeb56e76bd2f5ae56e3a42d6b85f6
sathwikv2005 <sathwikv2005@gmail.com>
2026-06-12 05:28:42 +0530

    add debug support for missing op_codes

--- src/debug.c
+++ src/debug.c
@@ -92,6 +92,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return constantInstruction("OP_GET_PROTERTY", chunk, offset);
         case OP_SET_PROPERTY:
             return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+        case OP_GET_INDEX:
+            return simpleInstruction("OP_GET_INDEX", offset);
+        case OP_SET_INDEX:
+            return simpleInstruction("OP_SET_INDEX", offset);
         case OP_GET_UPVALUE:
             return byteInstruction("OP_GET_UPVALUE", chunk, offset);
         case OP_SET_UPVALUE:
