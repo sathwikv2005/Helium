afff8c897f03758bc0dd19813172304f82bbba92
sathwikv2005 <sathwikv2005@gmail.com>
2026-06-20 12:04:30 +0530

    feat(natives): implement int() native function

--- src/native/type.c
+++ src/native/type.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdlib.h>
 
 #include "native_common.h"
@@ -35,7 +36,34 @@ static Value numberNative(int argCount, Value* args) {
     return NUMBER_VAL(value);
 }
 
+static Value intNative(int argCount, Value* args) {
+    if (argCount != 1) {
+        runtimeError("int() expects 1 argument.");
+        return NULL_VAL;
+    }
+    if (IS_NUMBER(args[0])) {
+        return NUMBER_VAL(trunc(AS_NUMBER(args[0])));
+    }
+    if (IS_STRING(args[0])) {
+        ObjString* str = AS_STRING(args[0]);
+
+        char* end;
+        double value = strtod(str->chars, &end);
+
+        if (end == str->chars) {
+            runtimeError("Invalid number: '%s'.", str->chars);
+            return NULL_VAL;
+        }
+
+        return NUMBER_VAL(trunc(value));
+    }
+
+    runtimeError("int() expects a number or a string");
+    return NULL_VAL;
+}
+
 void registerTypeNatives() {
     defineNative("string", stringNative);
+    defineNative("int", intNative);
     defineNative("number", numberNative);
 }
